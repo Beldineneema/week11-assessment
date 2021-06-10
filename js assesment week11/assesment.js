ac47511003927cf9b287b46f832a8812b40017b3
Beldineneema <79199587+Beldineneema@users.noreply.github.com>
2021-06-10 20:15:37 +0300

    Update assesment.js

--- js assesment week11/assesment.js
+++ js assesment week11/assesment.js
@@ -1,7 +1,45 @@
 // question 1
 
 
- }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <div id="Quiz">        
+        <form>
+            <label for="name">Student Name:</label><br>
+            <input type="text" id="name" name="name"><br>
+            <label for="class">Enter class:</label><br>
+            <input type="text" id="class" name="class"><br>
+                <label for="subject">Subject:</label><br>
+                <input type="text" id="subject" name="subject"><br>
+                <label for=" date">Date:</label><br>
+                <input type="date" id="date" name="date"><br>
+                <input type="button" onclick="quizResponses()" value="Submit">
+            </form>
+      </div>
+   <script>
+ function quizResponses(){
+      var name=document.getElementById("name").value;
+      var  className=document.getElementById("class").value;
+      var subject=document.getElementById("subject").value;
+      var date=document.getElementById("date").value;
+      if(name ==="" || className===""||subject===""||date===""){
+          alert("please fill in all the details!")
+      }
+      else{
+        alert(` You have submitted the following responses:  name:${name} , className:${className}, subject:${subject} , on this date${date}`)
+
+      }
+  } 
+   </script> 
+</body>
+<br>
+
+
+ 
 
  //question 2
 
