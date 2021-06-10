17f291aa4e38c215992fe4c6ebd81e469569c960
Beldineneema <79199587+Beldineneema@users.noreply.github.com>
2021-06-10 19:43:26 +0300

    Update assesment.js

--- js assesment week11/assesment.js
+++ js assesment week11/assesment.js
@@ -1,36 +1,35 @@
-// // question 1
-
-// function input(){
+// question 1
 
 
-// }
+ }
+
+ //question 2
+
+var girl = {
+    name : "Jane",
+    age : "20"
+};
 
 
-// //question 2
+//question 3
+//scopes of a varriable in js are declared by var keyword whic is its current execution context and closures.
 
-// var girl = {
-//     name : "Jane",
-//     age : "20"
-// };
-
-// //question 3
-// //scopes of a varriable in js are declared by var keyword whic is its current execution context and closures.
-
-// function foo() {
-//     var x = 1;
-//     function bar() {
-//       var y = 2;
-//       console.log(x); 
-//       console.log(y); 
-//     }
-//     bar();
-//     console.log(x);
-//     console.log(y);
-//   }
+function foo() {
+    var x = 1;
+    function bar() {
+      var y = 2;
+      console.log(x); 
+      console.log(y); 
+    }
+    bar();
+    console.log(x);
+    console.log(y);
+  }
   
-//   foo();
+  foo();
 
-// //   question 4
+
+//   question 4
 
 function one1()
 {
