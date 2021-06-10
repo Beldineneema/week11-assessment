7534623f1edbca397267ef9fcc778723221a946a
Beldineneema <79199587+Beldineneema@users.noreply.github.com>
2021-06-10 19:33:13 +0300

    Add files via upload

---
+++ js assesment week11/assesment.js
@@ -0,0 +1,40 @@
+// // question 1
+
+// function input(){
+
+
+// }
+
+
+// //question 2
+
+// var girl = {
+//     name : "Jane",
+//     age : "20"
+// };
+
+// //question 3
+// //scopes of a varriable in js are declared by var keyword whic is its current execution context and closures.
+
+// function foo() {
+//     var x = 1;
+//     function bar() {
+//       var y = 2;
+//       console.log(x); 
+//       console.log(y); 
+//     }
+//     bar();
+//     console.log(x);
+//     console.log(y);
+//   }
+  
+//   foo();
+
+// //   question 4
+
+function one1()
+{
+  return {
+      bar: "hello"
+  };
+}
